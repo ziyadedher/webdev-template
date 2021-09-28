729a319c4d6e04ad8a9eb413bbd935d572645d3b
Ziyad  Edher <ziyadedher@asana.com>
2021-09-27 17:40:20 -0700

    Write sample landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
+import Link from "next/link";
+
 import type { NextPage } from "next";
 
-const Index: NextPage = () => <p>Hello, world!</p>;
+const Index: NextPage = () => (
+  <div className="container flex flex-row justify-center items-center mx-auto h-screen">
+    <div className="text-center">
+      <h1 className="text-6xl text-gray-700">Hello, world!</h1>
+      <h2 className="pt-4">
+        Welcome to{" "}
+        <Link href="https://twitter.com/ziyadedher">
+          <a
+            href="https://twitter.com/ziyadedher"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-500"
+          >
+            @ziyadedher
+          </a>
+        </Link>
+        &rsquo;s webapp template.
+      </h2>
+    </div>
+  </div>
+);
 
 export default Index;
