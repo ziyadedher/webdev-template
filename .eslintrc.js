0658bebc0800707809125fed949fd006d6b378cb
Ziyad Edher <ziyad.edher@gmail.com>
2021-10-08 12:00:40 -0700

    Update ESLint configuration to work with TS imports (#52)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-commonjs -- exception for ESLint configuration file. */
+/* eslint-disable import/unambiguous -- exception for ESLint configuration file. */
+
 module.exports = {
   parserOptions: {
     project: "./tsconfig.json",
@@ -8,4 +11,21 @@ module.exports = {
     },
   },
   extends: ["@ziyadedher"],
+  rules: {
+    "react/require-default-props": "off",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        project: "./tsconfig.json",
+        alwaysTryTypes: true,
+      },
+    },
+    node: {
+      tryExtensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts", ".d.ts"],
+    },
+  },
 };
