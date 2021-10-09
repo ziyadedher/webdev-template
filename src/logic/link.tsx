615e723f31c3692c078c4e2ca7fa8e3375fe5564
Ziyad  Edher <ziyadedher@asana.com>
2021-10-09 10:52:27 -0700

    Revert some changes

--- src/logic/link.tsx
+++ src/logic/link.tsx
@@ -11,7 +11,6 @@ interface LinkProps {
 const Link: React.FunctionComponent<LinkProps> = ({
   href,
   shouldOpenInNewPage = false,
-  isAllCaps = false,
   children = null,
 }: LinkProps) => (
   <NextLink href={href}>
@@ -20,7 +19,6 @@ const Link: React.FunctionComponent<LinkProps> = ({
       // eslint-disable-next-line react/jsx-props-no-spreading -- unforunately, there's no cleaner way to do this that I can think of.
       {...(shouldOpenInNewPage ? { target: "_blank", rel: "noreferrer" } : {})}
     >
-      {isAllCaps ? "ALL CAPS!" : null}
       {children}
     </a>
   </NextLink>
